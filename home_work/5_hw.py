4a1dd83f24551be1d31b6f256f8fc6977dc31333
KovalVolk <kovalvolkova.e@gmail.com>
2026-03-11 09:17:12 +0300

    First commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -5,20 +5,20 @@ driver = webdriver.Chrome()
 driver.get("https://saucedemo.com/")
 
 #Поиск элемента
-icon = driver.find_element(By.CSS_SELECTOR, id = "user-name")
+icon = driver.find_element(By.CSS_SELECTOR, '#user-name')
 if icon is None:
     print('Элемент не найден')
 else:
     print('Элемент найден')
 
-icon1 = driver.find_element(By.CSS_SELECTOR, id = "password")
+icon1 = driver.find_element(By.CSS_SELECTOR, '#password')
 if icon is None:
     print('Элемент не найден')
 else:
     print('Элемент найден')
 
-icon2 = driver.find_element(By.CSS_SELECTOR, id = "login-button")
-if icon is None:
+btn = driver.find_element(By.CSS_SELECTOR, '#login-button')
+if btn is None:
     print('Элемент не найден')
 else:
     print('Элемент найден')
