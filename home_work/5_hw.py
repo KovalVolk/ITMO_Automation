5ecc6db53ee7185992c399ec19fdab14149ca269
KovalVolk <kovalvolkova.e@gmail.com>
2026-03-14 12:12:33 +0300

    First commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -5,17 +5,17 @@ from selenium.webdriver.common.by import By
 def check_elements('https://saucedemo.com/', username_locator, password_locator, login_locator):
 
 
-driver = webdriver.Chrome()
-driver.get("https://saucedemo.com/")
+    driver = webdriver.Chrome()
+    driver.get("https://saucedemo.com/")
 
 
-username_field = driver.find_element(By.CSS_SELECTOR, '#user-name')
+    username_field = driver.find_element(By.CSS_SELECTOR, '#user-name')
 
-password_field = driver.find_element(By.CSS_SELECTOR, '#password')
+    password_field = driver.find_element(By.CSS_SELECTOR, '#password')
 
-login_button = driver.find_element(By.CSS_SELECTOR, '#login-button')
+    login_button = driver.find_element(By.CSS_SELECTOR, '#login-button')
 
-if username_field and  password_field and login_button:
-    print('Элементы найдены')
-else:
-    print('Элементы не найдены')
+    if username_field and  password_field and login_button:
+       print('Элементы найдены')
+    else:
+       print('Элементы не найдены')
